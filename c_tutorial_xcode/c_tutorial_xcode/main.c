f68d146a53e703f2f43ec1d59fb7ab66412e3e77
Yunyun Huang <claudiayunyun@gmail.com>
2018-09-12 23:13:16 -0700

    remove xcodeproj files

---
+++ c_tutorial_xcode/c_tutorial_xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_tutorial_xcode
+//
+//  Created by Yunyun Huang on 9/12/18.
+//  Copyright © 2018 Yunyun Huang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
